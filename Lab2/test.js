65043281ce512a5189c986788922489cba3c6fda
Nguyen tan Tho <nguyentantho1302@gmail.com>
2018-04-05 15:06:43 -0500

    Lab3

--- Lab2/test.js
+++ Lab2/test.js
@@ -1,4 +1,6 @@
-
-setTimeout(() => { console.log('timeout'); }, 0);
-setImmediate(() => {console.log('immediate'); });
-process.nextTick(() => console.log('nexttick'));
+fs = require('fs')
+fs.readFile('.\Lab2\test.js', 'utf8', function (err,data) {
+    setTimeout(() => { console.log('timeout'); }, 0);
+    setImmediate(() => {console.log('immediate'); });
+    // process.nextTick(() => console.log('nexttick'));
+});
---
+++ Lab3/exercise1.js
@@ -0,0 +1,4 @@
+const dns = require('dns');
+dns.resolve4("www.mum.edu", function (err, addresses) {
+    console.log('addresses: ' + JSON.stringify(addresses));
+});
