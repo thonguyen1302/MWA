ee33381f2e441e1ca210950f0f23a9868110682c
986205@CS.MUM.EDU <986205@CS.MUM.EDU>
2018-04-05 15:22:34 -0500

    test

--- Lab2/test.js
+++ Lab2/test.js
@@ -4,3 +4,19 @@ fs.readFile('.\Lab2\test.js', 'utf8', function (err,data) {
     setImmediate(() => {console.log('immediate'); });
     // process.nextTick(() => console.log('nexttick'));
 });
+
+
+var courseName = 'WAA';
+var obj = {
+ courseName: 'MWA',
+ prerequisite: {
+ courseName: 'WAP',
+ getCourseName: function() {
+ return this.courseName;
+ }
+ }
+};
+var test = obj.prerequisite.getCourseName;
+console.log(test.bind(obj)());
+console.log(test.call(obj));
+console.log(test.apply(obj));
