d23f450996da3f0db4d5c4c7d8117f9dbac68848
Tho Nguyen <nguyentantho1302@gmail.com>
2018-04-05 20:40:32 -0500

    Lab2

--- Lab2/exercise2.js
+++ Lab2/exercise2.js
@@ -1,16 +1,39 @@
-function slow(callback) {
-  setTimeout(()=>{
-      for(let i=0; i<=5e8; i++){}
-  },0); 
-  if(Math.random() > 0.5) {
-    return callback("Error", null);
-  }
-  return callback(null, {id:12345})
+// Fix the slow function to be asynchronous/non-blocking
+function slow(callback){ 
+	setTimeout(()=>{
+    for(let i=0; i<= 5e8; i++){}
+  }, 0);
+	if (Math.random() > 0.5) { 	
+		return callback("Error",null) 
+	} 
+	return callback(null, {id:12345}) 
+} 
+
+function exec(fn){ 
+   // Complete the code here to implement chaining with callback
+    const callback = function(...restParam){
+        return restParam[0] === null ? restParam[1] : restParam[0];
+    }
+
+   const result = fn(callback);
+
+    return {
+        done: function(cb){
+            if(result !== "Error"){
+                cb(result);
+            }
+            return this;
+        },
+        fail: function(cb){
+
+            if(result === "Error"){
+                cb(result);
+            }
+
+            return this;
+        }
+    }
 }
 
-function exec(fn) {
-  //
-}
-
-exec(slow).done(function(data){console.log(data);})
-
+exec(slow).done(function(data){ console.log(data); })
+	    .fail(function(err){ console.log("Error: " + err); }); 
